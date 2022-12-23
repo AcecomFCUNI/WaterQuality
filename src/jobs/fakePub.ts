bd5318f8b3f701898466e88b7486336a276aa6a5
AnthonyLzq <sluzquinosa@uni.pe>
2022-12-22 23:11:48 -0500

    feat: updated fakePub to generate realistic values

--- src/jobs/fakePub.ts
+++ src/jobs/fakePub.ts
@@ -2,6 +2,9 @@ import { CronJob } from 'cron'
 import debug from 'debug'
 import { MqttClient } from 'mqtt'
 
+const randomInInterval = (min: number, max: number, fix = 2): string =>
+  (Math.random() * (max - min + 1) + min).toFixed(fix)
+
 const updateData = (client: MqttClient) => {
   return new CronJob('*/15 * * * * *', async (): Promise<void> => {
     const pubDebug = debug('WaterQuality:Mqtt:demo:pub')
@@ -11,24 +14,28 @@ const updateData = (client: MqttClient) => {
       pubDebug('Error: ', error)
     })
 
-    client.publish('WaterQuality/pH', `jhon/Chuzcarry/${Math.random()}`, () => {
-      client.publish(
-        'WaterQuality/totalDissolvedSolids',
-        `jhon/Chuzcarry/${Math.random()}`,
-        () => {
-          client.publish(
-            `WaterQuality/temperature`,
-            `jhon/Chuzcarry/${Math.random()}`,
-            () => {
-              client.publish(
-                'WaterQuality/turbidity',
-                `jhon/Chuzcarry/${Math.random()}`
-              )
-            }
-          )
-        }
-      )
-    })
+    client.publish(
+      'WaterQuality/pH',
+      `jhon/Chuzcarry/${randomInInterval(6.5, 7.5, 1)}`,
+      () => {
+        client.publish(
+          'WaterQuality/totalDissolvedSolids',
+          `jhon/Chuzcarry/${randomInInterval(100, 200, 0)}`,
+          () => {
+            client.publish(
+              `WaterQuality/temperature`,
+              `jhon/Chuzcarry/${randomInInterval(22, 25, 1)}`,
+              () => {
+                client.publish(
+                  'WaterQuality/turbidity',
+                  `jhon/Chuzcarry/${randomInInterval(10, 30, 0)}`
+                )
+              }
+            )
+          }
+        )
+      }
+    )
   })
 }
 
