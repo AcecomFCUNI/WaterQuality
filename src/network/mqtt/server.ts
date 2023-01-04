4c2faf708e17214b6c284177a4525edcc0920720
AnthonyLzq <sluzquinosa@uni.pe>
2023-01-04 01:30:10 -0500

    feat: improved global managment of mqtt server

--- src/network/mqtt/server.ts
+++ src/network/mqtt/server.ts
@@ -1,7 +1,11 @@
 import mqtt from 'mqtt'
 import { Debugger } from 'debug'
 
-let client: mqtt.MqttClient
+declare global {
+  // eslint-disable-next-line no-var
+  var __mqttClient__: mqtt.MqttClient
+}
+
 const options: mqtt.IClientOptions = {
   port: process.env.MQTT_PORT ? parseInt(process.env.MQTT_PORT) : 0,
   host: process.env.MQTT_HOST,
@@ -20,17 +24,17 @@ const connectedMessage = 'Connected to MQTT server.'
 const disconnectedMessage = 'Disconnected from MQTT server.'
 
 const getClient = (d?: Debugger) => {
-  if (!client) {
-    client = mqtt.connect(options)
-    client.on('connect', () => {
+  if (!global.__mqttClient__) {
+    global.__mqttClient__ = mqtt.connect(options)
+    global.__mqttClient__.on('connect', () => {
       d?.(connectedMessage)
     })
-    client.on('disconnect', () => {
+    global.__mqttClient__.on('disconnect', () => {
       d?.(disconnectedMessage)
     })
   }
 
-  return client
+  return global.__mqttClient__
 }
 
 const start = async (d: Debugger) => {
